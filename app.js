2ea9b359987a208be036c5082211124236136244
Scott Graham <scottgrahamdev@gmail.com>
2022-06-25 17:22:25 -0500

    Added hover affects for rating values and submit button. Started JS code, added click functionality to rating values

---
+++ app.js
@@ -0,0 +1,74 @@
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector(".three");
+const four = document.querySelector(".four");
+const five = document.querySelector(".five");
+
+const oneColor = document.querySelector(".one-color");
+const twoColor = document.querySelector(".two-color");
+const threeColor = document.querySelector(".three-color");
+const fourColor = document.querySelector(".four-color");
+const fiveColor = document.querySelector(".five-color");
+
+function whenClickedOne() {
+    let removeClassArr = [two, three, four, five]
+    for (i = 0; i < removeClassArr.length; i++) {
+        removeClassArr[i].classList.remove("whenClickedBg")
+    }
+    
+    one.classList.add("whenClickedBg");
+    oneColor.classList.add("whenClickedFont");
+}
+
+function whenClickedTwo() {
+    let removeClassArr = [three, four, five]
+    for (i = 0; i < removeClassArr.length; i++) {
+        removeClassArr[i].classList.remove("whenClickedBg")
+    }
+    
+    one.classList.add("whenClickedBg");
+    oneColor.classList.add("whenClickedFont");
+    two.classList.add("whenClickedBg");
+    twoColor.classList.add("whenClickedFont");
+}
+
+function whenClickedThree() {
+    let removeClassArr = [four, five]
+    let addClassArr = [one, two, three]
+    
+    for (i = 0; i < removeClassArr.length; i++) {
+        removeClassArr[i].classList.remove("whenClickedBg")
+    }
+
+    for (i = 0; i < addClassArr.length; i++) {
+        addClassArr[i].classList.add("whenClickedBg")
+    }
+}
+
+function whenClickedFour() {
+    five.classList.remove("whenClickedBg");
+    
+    let addClassArr = [one, two, three, four]
+    
+    for (i = 0; i < addClassArr.length; i++) {
+        addClassArr[i].classList.add("whenClickedBg")
+    }
+}
+
+function whenClickedFive() {
+    let addClassArr = [one, two, three, four, five]
+    for (i = 0; i < addClassArr.length; i++) {
+        addClassArr[i].classList.add("whenClickedBg")
+    }
+}
+
+one.addEventListener("click", whenClickedOne)
+two.addEventListener("click", whenClickedTwo)
+three.addEventListener("click", whenClickedThree)
+four.addEventListener("click", whenClickedFour)
+five.addEventListener("click", whenClickedFive)
+
+
+
+
+
